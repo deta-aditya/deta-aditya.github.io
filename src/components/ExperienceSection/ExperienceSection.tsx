c369106371f62bf87fde7ca69bffa0a2f3df323e
Deta Aditya <muhammaddetaaditya@gmail.com>
2021-06-03 17:21:51 +0700

    Separated TimelineView from ExperienceSection

--- src/components/ExperienceSection/ExperienceSection.tsx
+++ src/components/ExperienceSection/ExperienceSection.tsx
@@ -1,29 +1,26 @@
 import React from 'react'
+import { TimelineView } from '../TimelineView'
 import './ExperienceSection.css'
 
 function ExperienceSection() {
   return (
     <section className="experience-section">
       <h1 className="section-heading">Experience</h1>
-      <div className="timeline">
-        <article className="timeline-item">
-          <div className="timeline-line"></div>
-          <div className="timeline-dot"></div>
-          <p className="timeline-range">2020 NOV - 2021 APR</p>
-          <main className="timeline-main">
-            <h2 className="title">IT Technical Manager</h2>
-            <p className="subtitle">LiveIn - Full Time</p>
-            <div className="timeline-tags">
-              <span className="tag-item">Project Management</span>
-              <span className="tag-item">Software Architecture</span>
-              <span className="tag-item">Test Driven Development</span>
-              <span className="tag-item">Go</span>
-              <span className="tag-item">Laravel</span>
-              <span className="tag-item">iOS</span>
-            </div>
-          </main>
-        </article>
-      </div>
+      <TimelineView>
+        <TimelineView.Item 
+          range="2020 NOV - 2021 APR"
+          title="IT Technical Manager"
+          subtitle="LiveIn - Full Time"
+          tags={[
+            "Project Management",
+            "Software Architecture",
+            "Test Driven Development",
+            "Go",
+            "Laravel",
+            "iOS",
+          ]}
+        /> 
+      </TimelineView>
     </section>
   )
 }
