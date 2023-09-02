c545f67fc195ac78536ae3b057e546707d0254b8
deta-aditya <deta.aditya@tokopedia.com>
2023-09-02 16:46:20 +0700

    init gh pages with jekyll

--- src/model/experience/experience-data-providers.ts
+++
@@ -1,6 +0,0 @@
-import { ExperienceDataProvider, ExperienceJSON } from "./experience-types"
-import experiences from '../../data/experience.json'
-
-export const getFromLocalJSON: ExperienceDataProvider = () => {
-  return Promise.resolve(experiences as ExperienceJSON[])
-}
