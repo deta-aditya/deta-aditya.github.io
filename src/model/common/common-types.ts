04603e1687ba7d8ab1879f0564b1252fc25c8c40
Deta Aditya <muhammaddetaaditya@gmail.com>
2025-09-27 15:04:16 +0700

    Remake to Astro

--- src/model/common/common-types.ts
+++
@@ -1,6 +0,0 @@
-export type State<T> = 
-| { state: 'INITIAL' }
-| { state: 'LOADING' }
-| { state: 'SUCCESS', data: T }
-
-export type DataProvider<T> = () => Promise<T>
--- src/model/experience/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './experience-data-providers'
-export * from './experience-factories'
-export * from './experience-hooks'
-export * from './experience-types'
